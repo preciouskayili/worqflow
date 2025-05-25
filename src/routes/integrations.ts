f9c840e03d978a8e1565ad338f5e4a585f057fc2
Precious Kayili <preciouskayili@gmail.com>
2025-05-25 17:30:07 +0100

    Add name parameter to integrations api

--- src/routes/integrations.ts
+++ src/routes/integrations.ts
@@ -8,8 +8,21 @@ const router = Router();
 router.get("/", requireAuth, async (req: AuthRequest, res) => {
   try {
     const userId = req.user._id;
-    const integrations = await IntegrationModel.find({ user_id: userId });
-    res.status(200).json({ integrations });
+    const { name } = req.query as { name?: string };
+
+    if (name) {
+      const integration = await IntegrationModel.findOne({
+        $and: [{ name: name }, { user_id: userId }],
+      });
+      if (!integration) {
+        res.status(404).json({ message: "Integration not found" });
+      } else {
+        res.status(200).json({ integration });
+      }
+    } else {
+      const integrations = await IntegrationModel.find({ user_id: userId });
+      res.status(200).json({ integrations });
+    }
   } catch (err) {
     res.status(500).json({ message: "Failed to fetch integrations" });
   }
