9b4273dd4df7ed2bfe9c0bd29566b3581dcd1954
Precious Kayili <preciouskayili@gmail.com>
2025-06-26 17:38:15 +0100

    Add Linear tools for user and issue management. Implement functions to retrieve users by name and email, list teams, and get issues assigned to users. Remove unused Zoom tool to streamline the codebase.

--- src/tools/linear.ts
+++ src/tools/linear.ts
@@ -65,6 +65,126 @@ export const commentOnLinearIssue = tool({
   },
 });
 
+export const getLinearUserByName = tool({
+  name: "get_linear_user_by_name",
+  description: "Get a Linear user by name",
+  parameters: z.object({
+    name: z.string(),
+  }),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.users({
+      first: 1,
+      filter: {
+        name: { eq: args.name },
+      },
+    });
+    return res.nodes[0];
+  },
+});
+
+export const getLinearUserByEmail = tool({
+  name: "get_linear_user_by_email",
+  description: "Get a Linear user by email",
+  parameters: z.object({
+    email: z.string(),
+  }),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.users({
+      first: 1,
+      filter: {
+        email: { eq: args.email },
+      },
+    });
+    return res.nodes[0];
+  },
+});
+
+export const getLinearUserIssues = tool({
+  name: "get_linear_user_issues",
+  description: "Get all the issues assigned to a Linear user",
+  parameters: z.object({
+    userName: z.string(),
+  }),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.issues({
+      filter: { assignee: { name: { eq: args.userName } } },
+    });
+    return res.nodes;
+  },
+});
+
+export const getLinearIssueByTitle = tool({
+  name: "get_linear_issue_by_title",
+  description: "Get a Linear issue by title",
+  parameters: z.object({
+    title: z.string(),
+  }),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.issues({
+      filter: { title: { eq: args.title } },
+    });
+    return res.nodes;
+  },
+});
+
+export const getLinearTeamByName = tool({
+  name: "get_linear_team_by_name",
+  description: "Get a Linear team by name",
+  parameters: z.object({
+    name: z.string(),
+  }),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.teams({
+      first: 1,
+      filter: { name: { eq: args.name } },
+    });
+    return res.nodes[0];
+  },
+});
+
+export const getLinearTeamById = tool({
+  name: "get_linear_team_by_id",
+  description: "Get a Linear team by id",
+  parameters: z.object({
+    id: z.string(),
+  }),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.team(args.id);
+    return res;
+  },
+});
+
+export const listLinearTeams = tool({
+  name: "list_linear_teams",
+  description: "List all Linear teams",
+  parameters: z.object({}),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.teams();
+    return res.nodes;
+  },
+});
+
 export const updateLinearIssueStatus = tool({
   name: "update_linear_issue_status",
   description: "Update the status of a Linear issue",
@@ -80,6 +200,78 @@ export const updateLinearIssueStatus = tool({
       stateId: args.stateId,
     });
     return res.issue;
+  },
+});
+
+export const getTodoIssuesByTeamId = tool({
+  name: "get_todo_issues_by_team_id",
+  description: "Get all the todo issues by team id",
+  parameters: z.object({
+    teamId: z.string(),
+  }),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.issues({
+      filter: {
+        team: { id: { eq: args.teamId } },
+        state: { name: { eq: "Todo" } },
+      },
+    });
+    return res.nodes;
+  },
+});
+
+export const getUnassignedIssuesByTeamId = tool({
+  name: "get_unassigned_issues_by_team_id",
+  description: "Get all the unassigned issues by team id",
+  parameters: z.object({
+    teamId: z.string(),
+  }),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.issues({
+      filter: {
+        team: { id: { eq: args.teamId } },
+        assignee: { isMe: { eq: false } },
+      },
+    });
+    return res.nodes;
+  },
+});
+
+export const getLinearIssue = tool({
+  name: "get_linear_issue",
+  description: "Get a Linear issue",
+  parameters: z.object({
+    issueId: z.string(),
+  }),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.issue(args.issueId);
+    return res;
+  },
+});
+
+export const getAssignedLinearIssues = tool({
+  name: "get_assigned_linear_issues",
+  description: "Get all Linear issues assigned to the user",
+  parameters: z.object({}),
+  async execute(args, runContext?: RunContext<UserInfo>) {
+    const linear = await getLinearClient(
+      runContext?.context.userId.toString()!
+    );
+    const res = await linear.issues({
+      filter: {
+        assignee: { isMe: { eq: true } },
+      },
+    });
+    return res.nodes;
   },
 });
 
